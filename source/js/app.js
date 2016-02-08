06145bf44ae57dcb811fbcee902546a2ba31c06a
Punit Shah <punit.shah@bbc.co.uk>
2016-02-08 18:13:11 +0000

    added still poster images for mobile video

--- source/js/app.js
+++ source/js/app.js
@@ -5,13 +5,13 @@ define([
     'scrollHandler'
 ], function (news, React, Video, scrollHandler) {
     var videos = [
-        {selector: 'story-media-2-faux', videoId: 'p03hdp5k', background: true, autoplay: true},
+        {selector: 'story-media-2-faux', videoId: 'p03hdp5k', background: true, autoplay: true, poster: 'http://www.stage.bbc.co.uk/news/special/2016/newsspec_13329/content/english/img/3_europe/europe_poster.jpg'},
         {selector: 'story-media-2', videoId: 'p03h7p1t'},
-        {selector: 'story-media-3-faux', videoId: 'p03hdpwr', background: true, autoplay: true},
+        {selector: 'story-media-3-faux', videoId: 'p03hdpwr', background: true, autoplay: true, poster: 'http://www.stage.bbc.co.uk/news/special/2016/newsspec_13329/content/english/img/4_asia/asia_poster.jpg'},
         {selector: 'story-media-3', videoId: 'p03hdclq'},
-        {selector: 'story-media-5-faux', videoId: 'p03hdpct', background: true, autoplay: true},
+        {selector: 'story-media-5-faux', videoId: 'p03hdpct', background: true, autoplay: true, poster: 'http://www.stage.bbc.co.uk/news/special/2016/newsspec_13329/content/english/img/6_south_america/south_america_poster.jpg'},
         {selector: 'story-media-5', videoId: 'p03hdccz'},
-        {selector: 'story-media-7-faux', videoId: 'p03hdpzw', background: true, autoplay: true},
+        {selector: 'story-media-7-faux', videoId: 'p03hdpzw', background: true, autoplay: true, poster: 'http://www.stage.bbc.co.uk/news/special/2016/newsspec_13329/content/english/img/7_north_america/north_america_poster.jpg'},
         {selector: 'story-media-7', videoId: 'p03hdcdj'}
     ];
 
@@ -25,7 +25,7 @@ define([
     }
 
     scrollHandler.init();
-    
+
     $.emit('init_images');
     news.sendMessageToRemoveLoadingImage();
 });
