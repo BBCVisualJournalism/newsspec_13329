bda6efa9997d3cddbdde83616033f12dd24f2f4e
Punit Shah <punit.shah@bbc.co.uk>
2016-02-11 11:01:17 +0000

    updated vpid for andy's video

--- source/js/app.js
+++ source/js/app.js
@@ -11,7 +11,7 @@ define([
     if (locale === 'en-GB') {
         var videos = [
             {selector: 'story-media-2-faux', videoId: 'p03hdp5k', background: true, autoplay: true, poster: 'http://www.stage.bbc.co.uk/news/special/2016/newsspec_13329/content/english/img/3_europe/europe_poster.jpg'},
-            {selector: 'story-media-2', videoId: 'p03h7p1t'},
+            {selector: 'story-media-2', videoId: 'p03j1t2f'},
             {selector: 'story-media-3-faux', videoId: 'p03hdpwr', background: true, autoplay: true, poster: 'http://www.stage.bbc.co.uk/news/special/2016/newsspec_13329/content/english/img/4_asia/asia_poster.jpg'},
             {selector: 'story-media-3', videoId: 'p03hdclq'},
             {selector: 'story-media-5-faux', videoId: 'p03hdpct', background: true, autoplay: true, poster: 'http://www.stage.bbc.co.uk/news/special/2016/newsspec_13329/content/english/img/6_south_america/south_america_poster.jpg'},
